14b2bb5f19f83289931ba5f17bee178f15d4e28a
suyi-wyh <1227081317@qq.com>
2020-11-26 00:04:46 +0800

    平时练习

--- work_pratice/work_pratice.c
+++ work_pratice/work_pratice.c
@@ -1,8 +1,7 @@
 ﻿// work_pratice.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <stdio.h>
-#include<stdlib.h>
+#include"mystring.h"
 
 void yanghuisanjiao(int num)
 {
@@ -57,14 +56,24 @@ void GuessNum()
                             }
                         }
 }
+
 int main()
 {
+
+    char buffer1[] = "ABCD";
+    char buffer2[] = "DCBA";
+    /*
     yanghuisanjiao(9);
     printf("%c\n", Guess());
     printf("----------\n");
     GuessNum();
-}
+    x=IsCmpString(buffer1, buffer2);
+    printf("%d\n",x);
+    */
 
+    char* p = zuoxuan(buffer1, 2);
+   // printf("%s", p);
+}
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
@@ -75,3 +84,4 @@ int main()
 //   4. 使用错误列表窗口查看错误
 //   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
