5d79867241cb085b39bde8c324d4abdc99a4f78a
suyi-wyh <1227081317@qq.com>
2021-01-12 21:40:07 +0800

    二叉树

---
+++ mine/myBinaryTree/mybinarytree.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct BinaryTreeNode
+{
+	int _val;
+	struct BinaryTreeNode* _left;
+	struct BinaryTreeNode* _right;
+}BinaryTreeNode;
+
+BinaryTreeNode* InitTree(int *a,int nums)  //建树
+{
+	BinaryTreeNode* root;
+	root = NULL;
+	while ()
+	{
+
+	}
+	return root;
+}
+
+void Inorder(BinaryTreeNode *root)    //中序遍历
+{
+	if (root != NULL)
+	{
+		Inorder(root->_left);
+		printf("%d\n",root->_val);
+		Inorder(root->_right);
+	}
+}
+
+void PrevOrder(BinaryTreeNode *root)   // 前序遍历
+{
+	if (NULL != root)
+	{
+		printf("%d\n",root->_val);
+		PrevOrder(root->_left);
+		PrevOrder(root->_right);
+	}
+}
+
+void PostOrder(BinaryTreeNode *root)  // 后序遍历
+{
+	if (NULL != root)
+	{
+		PostOrder(root->_left);
+		PostOrder(root->_right);
+		printf("%d\n", root->_val);
+	}
+}
+
+int TreeSize(BinaryTreeNode *root)  //求树大小
+{
+	if (root == NULL)
+		return 0;
+	return TreeSize(root->_right)+TreeSize(root->_left)+1;
+
+}
+
+int main()
+{
+	return 0;
+}
--- work_pratice/mystring.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-
-int IsCmpString(char *a,char *b) ;   //是否为旋转后
-
-char* zuoxuan(char a[], int x);//左旋x个字符
- 
