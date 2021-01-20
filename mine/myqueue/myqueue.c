7ba0453acf0ab09b9ec5d414405ddc22b3b266dd
suyi-wyh <1227081317@qq.com>
2021-01-21 03:57:57 +0800

    链表队列

--- mine/myqueue/myqueue.c
+++ mine/myqueue/myqueue.c
@@ -1,77 +1,112 @@
 #include"myqueue.h"
 
 
-
-
 int QueueEmpty(queue* que)
 {
-	if (NULL == que)
+	if (NULL == que->_front)
 		return 1;
 	else
 		return 0;
 }
 
-void QueueInit(queue* que)
+queue* QueueInit()
 {
-	que = (queue*)malloc(sizeof(queue));
-	que = NULL;
+	queue* que = (queue*)malloc(sizeof(queue));
+	que->_front = que->_tail = NULL;
+	return que;
 }
 
-void QueuePush(queue* que, QUEUE_TYPE num)
-{
-	if (NULL == que->_front)
-		que->_front = num;
+void QueuePush(queue* que, QUEUE_TYPE num)        //入队
+{ 
+	if (que->_front == NULL)
+	{
+		que->_front = (queuenode*)malloc(sizeof(queuenode));
+		que->_front->_data = num;
+		que->_front->_next = NULL;
+		que->_tail = que->_front;
+	}
 	else
 	{
-		queue* newnode = (queue*)malloc(sizeof(queue));
-		newnode->_front = num;
-		newnode->next = NULL;
-		que->next = newnode;
+		queuenode* newnode = (queuenode*)malloc(sizeof(queuenode));
+		newnode->_data = num;
+		newnode->_next = NULL;
+		que->_tail->_next = newnode;
+		que->_tail=que->_tail->_next;
 	}
+	
 }
 
-void QueuePop(queue* que)
+void QueuePop(queue* que)     //出队
 {
-	if (!QueueEmpty(que))
+	if (NULL == que->_front && que->_front == que->_tail)
 		return;
+
+	if (NULL != que->_front && que->_front == que->_tail)
+	{
+		free(que->_front);
+		que->_front = que->_tail = NULL;
+	}
+
 	else
-		que = que->next;
+		que->_front = que->_front->_next;
 }
 
-QUEUE_TYPE QueueFront(queue* que)
+queuenode* QueueFront(queue* que)    //获取队首元素
 {
 	return que->_front;
 }
-int QueueNums(queue* que)
+
+
+int QueueNums(queue* que)   //获取队列长度  
 {
 	int nums = 0;
-	while (que)
+	queuenode* cur = que->_front;
+	while (cur)
 	{
 		++nums;
-		que = que->next;
+		cur = cur->_next;
 	}
 	return nums;
 }
-QUEUE_TYPE QueueBack(queue* que)
+
+
+queuenode* QueueBack(queue* que)    //获取队尾元素
 {
-	queue* newnode = que;
-	while (newnode->next)
+	queuenode* cur = que->_front;
+	while (cur->_next)
 	{
-		newnode = newnode->next;
+		cur = cur->_next;
 	}
-	return newnode->_front;
+	return cur;
 }
 
-void QueueDestory(queue* que)
+void QueueDestory(queue* que)   //销毁队列
 {
 	if (NULL == que)
 		return;
 	else
-		while (que)
-			QueuePop(que);
+	{
+		while (que->_front)
+		{
+			queuenode* next = que->_front->_next;
+			free(que->_front);
+			que->_front = next;
+		}
+		que->_tail = que->_front;
+	}
 }
 
 int main()
-{
-
+{	
+	queue* q = QueueInit();
+	QueuePush(q,1);
+	QueuePush(q,2);
+	printf("%d  \n",QueueFront(q)->_data);
+	printf("%d  \n",QueueFront(q)->_data);
+	QueuePop(q);
+	printf("%d  \n", QueueFront(q)->_data);
+	QueuePop(q);
+	printf("%d  \n", QueueFront(q)->_data);
+	QueuePop(q);
+	return 0;
 }
--- mine/myqueue/myqueue.h
+++ mine/myqueue/myqueue.h
@@ -7,16 +7,22 @@ typedef int QUEUE_TYPE;
 
 typedef struct queuenode
 {
-	QUEUE_TYPE _front;
-	struct queuenode* next;
+	QUEUE_TYPE _data;
+	struct queuenode* _next;
+}queuenode;
+
+typedef struct queue
+{
+	queuenode* _front;
+	queuenode* _tail;
 }queue;
 
 
-void QueueInit(queue* que);
+queue* QueueInit();
 int QueueEmpty(queue *que);
 void QueuePush(queue *que ,QUEUE_TYPE num);
 void QueuePop(queue *que);
 void QueueDestory(queue *que);
-QUEUE_TYPE QueueFront(queue *que);
-QUEUE_TYPE QueueBack(queue *que);
+queuenode* QueueFront(queue* que);
+queuenode* QueueBack(queue *que);
 int QueueNums(queue* que);
