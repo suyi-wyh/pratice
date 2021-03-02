b7357d54820e374dd0570a1244c7ba080876e472
suyi-wyh <1227081317@qq.com>
2021-03-02 17:48:48 +0800

    304 二维区域和检索-矩阵不可变

--- c/leetcode_pratice/leetcode_pratice.cpp
+++ c/leetcode_pratice/leetcode_pratice.cpp
@@ -112,6 +112,44 @@ void numArrayFree(NumArray* obj) {
 }
 
 
+
+/// <summary>
+///  304 二维区域和检索-矩阵不可变
+/// </summary>
+typedef struct {
+    int** nums;
+    int col;
+    int row;
+} NumMatrix;
+
+
+NumMatrix* numMatrixCreate(int** matrix, int matrixSize, int* matrixColSize) {
+    NumMatrix* numsMatrix = (NumMatrix*)malloc(sizeof(NumMatrix));
+    numsMatrix->col = matrixSize;
+    numsMatrix->nums = (int**)malloc(numsMatrix->col * sizeof(int*));
+    for (int i = 0; i < numsMatrix->col; ++i)
+    {
+        numsMatrix->row = matrixColSize[i] + 1;
+        numsMatrix->nums[i] = (int*)malloc(numsMatrix->row * sizeof(int));
+        numsMatrix->nums[i][0] = 0;
+        for (int j = 1; j < numsMatrix->row; ++j)
+            numsMatrix->nums[i][j] = numsMatrix->nums[i][j - 1] + matrix[i][j - 1];
+    }
+    return numsMatrix;
+}
+
+int numMatrixSumRegion(NumMatrix* obj, int row1, int col1, int row2, int col2) {
+    int nums = 0;
+    for (int i = row1; i <= row2; ++i)
+        nums += (obj->nums[i][col2 + 1] - obj->nums[i][col1]);
+    return nums;
+}
+
+void numMatrixFree(NumMatrix* obj) {
+    for (int i = 0; i < obj->col; ++i)
+        free(obj->nums[i]);
+    free(obj->nums);
+}
 /// <summary>
 /// 395 至少有K歌重复字符的最长子串  
 /// </summary>
