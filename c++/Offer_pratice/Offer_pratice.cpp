e058961aa20714a972a8fc2efc93986fe698c231
suyi-wyh <1227081317@qq.com>
2021-03-21 10:00:13 +0800

    73  矩阵置零

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -274,6 +274,50 @@ public:
     }
 
     /// <summary>
+    /// 73 矩阵置零
+    /// </summary>
+    /// <param name="matrix"></param>
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool col = false, row = false;
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0)
+                col = true;
+        }
+
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0)
+                row = true;
+        }
+
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+
+
+        if (col) {
+            for (int i = 0; i < n; ++i)
+                matrix[0][i] = 0;
+        }
+
+        if (row) {
+            for (int i = 0; i < m; ++i)
+                matrix[i][0] = 0;
+        }
+    }
+    /// <summary>
     /// 92 反转链表Ⅱ
     /// </summary>
     /// <param name="head"></param>
