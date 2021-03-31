922f183b70964f87dd80b5752e05c11e32b63c27
suyi-wyh <1227081317@qq.com>
2021-03-31 14:19:54 +0800

    90 子集Ⅱ

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -8,7 +8,7 @@
 #include<StlLock.h>
 #include<vector>
 #include <unordered_set>
-  
+#include<algorithm>
 using namespace std;    
 
 struct ListNode {
@@ -478,6 +478,8 @@ public:
 
         return head;
     }
+
+
     /// <summary>
     /// 92 反转链表Ⅱ
     /// </summary>
@@ -852,7 +854,35 @@ public:
 	}
 };
 
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
 
+    void backtracking(vector<int>& nums, int startIndex, vector<bool>& used) {
+        result.push_back(path);
+        for (int i = startIndex; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false) {
+                continue;
+            }
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtracking(nums, i + 1, used);
+            used[i] = false;
+            path.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end()); // 去重需要排序
+        backtracking(nums, 0, used);
+        return result;
+    }
+};
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
