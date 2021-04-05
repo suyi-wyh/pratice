5cbbf57dc7f6626820a7619d74dd064487530c9f
suyi-wyh <1227081317@qq.com>
2021-04-05 15:15:40 +0800

    88 合并两个有序数组

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -481,6 +481,34 @@ public:
 
 
     /// <summary>
+    /// 88 合并两个有序数组
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="m"></param>
+    /// <param name="nums2"></param>
+    /// <param name="n"></param>
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m + n - 1;
+        m--;
+        n--;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] >= nums2[n]) {
+                nums1[i] = nums1[m];
+
+                i--; m--;
+            }
+            else {
+                nums1[i] = nums2[n];
+                i--; n--;
+            }
+        }
+        while (n >= 0) {
+            nums1[i] = nums2[n];
+            i--; n--;
+        }
+
+    }
+    /// <summary>
     /// 92 反转链表Ⅱ
     /// </summary>
     /// <param name="head"></param>
