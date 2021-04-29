b076e83d8bbc6fdea403b689bd4e7875fea76a85
suyi-wyh <1227081317@qq.com>
2021-04-29 20:34:05 +0800

    403 青蛙过河

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -1408,6 +1408,34 @@ public:
         return true;
     }
 
+	/// <summary>
+	/// 403 青蛙过河
+	/// </summary>
+	/// <param name="stones"></param>
+	/// <returns></returns>
+	bool canCross(vector<int>& stones) {
+		int n = stones.size();
+		vector<vector<int>> dp(n, vector<int>(n));
+		dp[0][0] = true;
+		for (int i = 1; i < n; ++i) {
+			if (stones[i] - stones[i - 1] > i) {
+				return false;
+			}
+		}
+		for (int i = 1; i < n; ++i) {
+			for (int j = i - 1; j >= 0; --j) {
+				int k = stones[i] - stones[j];
+				if (k > j + 1) {
+					break;
+				}
+				dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+				if (i == n - 1 && dp[i][k]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
     /// <summary>
     /// 541 反转字符串Ⅱ
     /// </summary>
