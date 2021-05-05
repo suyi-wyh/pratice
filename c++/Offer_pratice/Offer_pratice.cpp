1b7e4a01bfadcfb4dd74254a479e07e1f3ab46e3
suyi-wyh <1227081317@qq.com>
2021-05-05 20:10:40 +0800

    740 删除并获得点数

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -1495,7 +1495,32 @@ public:
 		}
 		return false;
 	}
-	
+	/// <summary>
+	/// 740 删除并获得点数
+	/// </summary>
+	/// <param name="nums"></param>
+	/// <returns></returns>
+	int rob(vector<int>& nums) {
+		int size = nums.size();
+		int first = nums[0], second = max(nums[0], nums[1]);
+		for (int i = 2; i < size; i++) {
+			int temp = second;
+			second = max(first + nums[i], second);
+			first = temp;
+		}
+		return second;
+	}
+	int deleteAndEarn(vector<int>& nums) {
+		int maxVal = 0;
+		for (int val : nums) {
+			maxVal = max(maxVal, val);
+		}
+		vector<int> sum(maxVal + 1);
+		for (int val : nums) {
+			sum[val] += val;
+		}
+		return rob(sum);
+	}
     /// <summary>
     /// 781 森林中的兔子
     /// </summary>
