f28fe61294833f75bd0b8b0b2ee51550fde55225
suyi-wyh <1227081317@qq.com>
2021-05-08 16:56:41 +0800

    1723 完成所有工作的最短时间

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -1777,6 +1777,55 @@ public:
 		}
 		return arr;
 	}
+	
+	/// <summary>
+	/// 1723 完成所有工作得最短时间
+	/// </summary>
+	/// <param name="jobs"></param>
+	/// <param name="workloads"></param>
+	/// <param name="idx"></param>
+	/// <param name="limit"></param>
+	/// <returns></returns>
+	bool backtrack(vector<int>& jobs, vector<int>& workloads, int idx, int limit) {
+		if (idx >= jobs.size()) {
+			return true;
+		}
+
+		int cur = jobs[idx];
+		for (auto& workload : workloads) {
+			if (workload + cur <= limit) {
+				workload += cur;
+				if (backtrack(jobs, workloads, idx + 1, limit)) {
+					return true;
+				}
+				workload -= cur;
+			}
+			if (workload == 0 || workload + cur == limit) {
+				break;
+			}
+		}
+		return false;
+	}
+
+	bool check(vector<int>& jobs, int k, int limit) {
+		vector<int> workloads(k, 0);
+		return backtrack(jobs, workloads, 0, limit);
+	}
+
+	int minimumTimeRequired(vector<int>& jobs, int k) {
+		sort(jobs.begin(), jobs.end(), greater<int>());
+		int l = jobs[0], r = accumulate(jobs.begin(), jobs.end(), 0);
+		while (l < r) {
+			int mid = (l + r) >> 1;
+			if (check(jobs, k, mid)) {
+				r = mid;
+			}
+			else {
+				l = mid + 1;
+			}
+		}
+		return l;
+	}
     /// <summary>
     /// 面试题17.21
     /// </summary>
