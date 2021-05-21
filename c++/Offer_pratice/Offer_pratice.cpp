fb1d53ef45d3b5ae418e7396f467297e9c521ead
suyi-wyh <1227081317@qq.com>
2021-05-21 18:44:50 +0800

    1035 不相交的线

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -2035,6 +2035,27 @@ public:
 		}
 		return left;
 	}
+	
+	/// <summary>
+	/// 1035 不相交的线
+	/// </summary>
+	/// <param name="nums1"></param>
+	/// <param name="nums2"></param>
+	/// <returns></returns>
+	int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+		int n1 = nums1.size();
+		int n2 = nums2.size();
+		vector<vector<int>> ans(n1 + 1, vector<int>(n2 + 1));
+		for (int i = 1; i <= n1; ++i) {
+			for (int j = 1; j <= n2; ++j) {
+				if (nums1[i - 1] == nums2[j - 1])
+					ans[i][j] = ans[i - 1][j - 1] + 1;
+				else
+					ans[i][j] = max(ans[i][j - 1], ans[i - 1][j]);
+			}
+		}
+		return ans[n1][n2];
+	}
 	/// <summary>
 	/// 1047 删除字符串中的所有相邻重复项
 	/// </summary>
