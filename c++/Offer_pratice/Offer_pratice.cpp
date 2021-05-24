8d27e42ef6107232f7e58d9e602f1b4e08377e2f
suyi-wyh <1227081317@qq.com>
2021-05-24 20:31:07 +0800

    664  奇怪的打印机

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -1736,6 +1736,31 @@ public:
 		return false;
 	}
 	/// <summary>
+	/// 664 奇怪的打印机
+	/// </summary>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	int strangePrinter(string s) {
+		int n = s.length();
+		vector<vector<int>> f(n, vector<int>(n));
+		for (int i = n - 1; i >= 0; i--) {
+			f[i][i] = 1;
+			for (int j = i + 1; j < n; j++) {
+				if (s[i] == s[j]) {
+					f[i][j] = f[i][j - 1];
+				}
+				else {
+					int minn = INT_MAX;
+					for (int k = i; k < j; k++) {
+						minn = min(minn, f[i][k] + f[k + 1][j]);
+					}
+					f[i][j] = minn;
+				}
+			}
+		}
+		return f[0][n - 1];
+	}
+	/// <summary>
 	/// 692 前k个高频单词
 	/// </summary>
 	/// <param name="words"></param>
