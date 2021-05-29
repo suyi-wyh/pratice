f8e11169230bb4be791f3f5afc7156cc60b58dd4
suyi <1227081317@qq.com>
2021-05-29 20:04:23 +0800

    1074  元素和为目标值的子矩阵数量和

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -2165,7 +2165,40 @@ public:
 		return stk;
 	}
     
+	/// <summary>
+	/// 1074 元素和为目标值的子矩阵数量
+	/// </summary>
+	/// <param name="nums"></param>
+	/// <param name="k"></param>
+	/// <returns></returns>
+	int subarraySum(vector<int>& nums, int k) {
+		unordered_map<int, int> mp;
+		mp[0] = 1;
+		int count = 0, pre = 0;
+		for (auto& x : nums) {
+			pre += x;
+			if (mp.find(pre - k) != mp.end()) {
+				count += mp[pre - k];
+			}
+			mp[pre]++;
+		}
+		return count;
+	}
 
+	int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+		int ans = 0;
+		int m = matrix.size(), n = matrix[0].size();
+		for (int i = 0; i < m; ++i) { // 枚举上边界
+			vector<int> sum(n);
+			for (int j = i; j < m; ++j) { // 枚举下边界
+				for (int c = 0; c < n; ++c) {
+					sum[c] += matrix[j][c]; // 更新每列的元素和
+				}
+				ans += subarraySum(sum, target);
+			}
+		}
+		return ans;
+	}
     /// <summary>
     /// 1143 最长公共子序列
     /// </summary>
