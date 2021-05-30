51d0819036cadbfb7442e2f63df8ec3e1e2fc56f
suyi <1227081317@qq.com>
2021-05-30 19:23:11 +0800

    200  岛屿数量

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -1335,7 +1335,36 @@ public:
         return ret;
     }
 
+	/// <summary>
+	/// 200 岛屿数量
+	/// </summary>
+	/// <param name="grid"></param>
+	/// <param name="i"></param>
+	/// <param name="j"></param>
+	void dfs(vector<vector<char>>& grid, int i, int j) {
+		grid[i][j] = 0;
+		if (i + 1 < grid.size() && grid[i + 1][j] == '1')
+			dfs(grid, i + 1, j);
+		if (i - 1 >= 0 && grid[i - 1][j] == '1')
+			dfs(grid, i - 1, j);
+		if (j + 1 < grid[0].size() && grid[i][j + 1] == '1')
+			dfs(grid, i, j + 1);
+		if (j - 1 >= 0 && grid[i][j - 1] == '1')
+			dfs(grid, i, j - 1);
 
+	}
+	int numIslands(vector<vector<char>>& grid) {
+		int ret = 0;
+		for (int i = 0; i < grid.size(); ++i) {
+			for (int j = 0; j < grid[0].size(); ++j) {
+				if (grid[i][j] == '1') {
+					ret++;
+					dfs(grid, i, j);
+				}
+			}
+		}
+		return ret;
+	}
     /// <summary>
     /// 227 基本计算器
     /// </summary>
