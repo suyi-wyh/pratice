bb9ea15659a822420b082f751f0e7bcccd35d084
suyi <1227081317@qq.com>
2021-06-18 16:38:19 +0800

    483 最小好进制

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -2054,6 +2054,27 @@ public:
 		return ret;
 	}
 	/// <summary>
+	/// 483 最小好进制
+	/// </summary>
+	/// <param name="n"></param>
+	/// <returns></returns>
+	string smallestGoodBase(string n) {
+		long nVal = stol(n);
+		int mMax = floor(log(nVal) / log(2));
+		for (int m = mMax; m > 1; m--) {
+			int k = pow(nVal, 1.0 / m);
+			long mul = 1, sum = 1;
+			for (int i = 0; i < m; i++) {
+				mul *= k;
+				sum += mul;
+			}
+			if (sum == nVal) {
+				return to_string(k);
+			}
+		}
+		return to_string(nVal - 1);
+	}
+	/// <summary>
 	/// 494 目标和
 	/// </summary>
 	/// <param name="nums"></param>
