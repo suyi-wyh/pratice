cf4e2b27b987956f74ced0a6b79f3e846c5ad2a1
suyi <1227081317@qq.com>
2021-06-30 22:55:51 +0800

    剑指offer 37 序列化二叉树

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -402,6 +402,59 @@ public:
         return ret * flag;
     }
 	/// <summary>
+	///  剑指offer 37 序列化二叉树
+	/// </summary>
+	/// <param name="root"></param>
+	/// <param name="str"></param>
+	void rserialize(TreeNode* root, string& str) {
+		if (root == nullptr) {
+			str += "None,";
+		}
+		else {
+			str += to_string(root->val) + ",";
+			rserialize(root->left, str);
+			rserialize(root->right, str);
+		}
+	}
+
+	string serialize(TreeNode* root) {
+		string ret;
+		rserialize(root, ret);
+		return ret;
+	}
+
+	TreeNode* rdeserialize(list<string>& dataArray) {
+		if (dataArray.front() == "None") {
+			dataArray.erase(dataArray.begin());
+			return nullptr;
+		}
+
+		TreeNode* root = new TreeNode(stoi(dataArray.front()));
+		dataArray.erase(dataArray.begin());
+		root->left = rdeserialize(dataArray);
+		root->right = rdeserialize(dataArray);
+		return root;
+	}
+
+	TreeNode* deserialize(string data) {
+		list<string> dataArray;
+		string str;
+		for (auto& ch : data) {
+			if (ch == ',') {
+				dataArray.push_back(str);
+				str.clear();
+			}
+			else {
+				str.push_back(ch);
+			}
+		}
+		if (!str.empty()) {
+			dataArray.push_back(str);
+			str.clear();
+		}
+		return rdeserialize(dataArray);
+	}
+	/// <summary>
 	///  剑指offer 38 字符串的排列
 	/// </summary>
 	/// <param name="s"></param>
