411cd82c0854d1d6b47f50136e40903569829391
suyi <1227081317@qq.com>
2021-07-03 17:32:22 +0800

    451 根据字符出现频率排序

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -2159,6 +2159,32 @@ public:
 		return false;
 	}
 	/// <summary>
+	/// 451 根据字符出现频率排序
+	/// </summary>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	string frequencySort(string s) {
+		unordered_map<char, int> mp;
+		int length = s.length();
+		for (auto& ch : s) {
+			mp[ch]++;
+		}
+		vector<pair<char, int>> vec;
+		for (auto& it : mp) {
+			vec.emplace_back(it);
+		}
+		sort(vec.begin(), vec.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
+			return a.second > b.second;
+			});
+		string ret;
+		for (auto& [ch, num] : vec) {
+			for (int i = 0; i < num; i++) {
+				ret.push_back(ch);
+			}
+		}
+		return ret;
+	}
+	/// <summary>
     /// 456 132模式
     /// </summary>
     /// <param name="nums"></param>
