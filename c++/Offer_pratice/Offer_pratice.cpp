4dd5c15d58307ca885f03d2d5680dc8bda537dbd
suyi <1227081317@qq.com>
2021-07-14 22:47:13 +0800

    1818 绝对差值和

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -3871,6 +3871,32 @@ public:
 		return answer;
 	}
 	/// <summary>
+	/// 1818 绝对差值和
+	/// </summary>
+	/// <param name="nums1"></param>
+	/// <param name="nums2"></param>
+	/// <returns></returns>
+	static constexpr int mod = 1'000'000'007;
+	
+	int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
+		vector<int> rec(nums1);
+		sort(rec.begin(), rec.end());
+		int sum = 0, maxn = 0;
+		int n = nums1.size();
+		for (int i = 0; i < n; i++) {
+			int diff = abs(nums1[i] - nums2[i]);
+			sum = (sum + diff) % mod;
+			int j = lower_bound(rec.begin(), rec.end(), nums2[i]) - rec.begin();
+			if (j < n) {
+				maxn = max(maxn, diff - (rec[j] - nums2[i]));
+			}
+			if (j > 0) {
+				maxn = max(maxn, diff - (nums2[i] - rec[j - 1]));
+			}
+		}
+		return (sum - maxn + mod) % mod;
+	}
+	/// <summary>
 	/// 1833 雪糕的最大数量
 	/// </summary>
 	/// <param name="costs"></param>
