ce937f016b1496d1bf7e1e69bb0bf69e9cc12cb2
suyi <1227081317@qq.com>
2021-07-28 14:15:17 +0800

    863 二叉树中所有距离为k的节点

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -4424,6 +4424,56 @@ public:
         return result;
     }
 };
+
+/// <summary>
+/// 863 二叉树中所有距离为k的节点
+/// </summary>
+class Solution {
+	unordered_map<int, TreeNode*> parents;
+	vector<int> ans;
+
+	void findParents(TreeNode* node) {
+		if (node->left != nullptr) {
+			parents[node->left->val] = node;
+			findParents(node->left);
+		}
+		if (node->right != nullptr) {
+			parents[node->right->val] = node;
+			findParents(node->right);
+		}
+	}
+
+	void findAns(TreeNode* node, TreeNode* from, int depth, int k) {
+		if (node == nullptr) {
+			return;
+		}
+		if (depth == k) {
+			ans.push_back(node->val);
+			return;
+		}
+		if (node->left != from) {
+			findAns(node->left, node, depth + 1, k);
+		}
+		if (node->right != from) {
+			findAns(node->right, node, depth + 1, k);
+		}
+		if (parents[node->val] != from) {
+			findAns(parents[node->val], node, depth + 1, k);
+		}
+	}
+
+public:
+	vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+		// 从 root 出发 DFS，记录每个结点的父结点
+		findParents(root);
+
+		// 从 target 出发 DFS，寻找所有深度为 k 的结点
+		findAns(target, nullptr, 0, k);
+
+		return ans;
+	}
+};
+
 /// <summary>
 /// 981 基于时间的键值存储
 /// </summary>
