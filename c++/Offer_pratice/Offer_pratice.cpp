8ffa65b046e13f206ad987c4a46e0eac9ec6cb69
suyi <1227081317@qq.com>
2021-08-05 21:19:58 +0800

    802 找到最终的安全状态

--- c++/Offer_pratice/Offer_pratice.cpp
+++ c++/Offer_pratice/Offer_pratice.cpp
@@ -3063,6 +3063,37 @@ public:
 		return ret;
 	}
 	/// <summary>
+	/// 802 找到最终安全状态
+	/// </summary>
+	/// <param name="graph"></param>
+	/// <returns></returns>
+	vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+		int n = graph.size();
+		vector<int> color(n);
+
+		function<bool(int)> safe = [&](int x) {
+			if (color[x] > 0) {
+				return color[x] == 2;
+			}
+			color[x] = 1;
+			for (int y : graph[x]) {
+				if (!safe(y)) {
+					return false;
+				}
+			}
+			color[x] = 2;
+			return true;
+		};
+
+		vector<int> ans;
+		for (int i = 0; i < n; ++i) {
+			if (safe(i)) {
+				ans.push_back(i);
+			}
+		}
+		return ans;
+	}
+	/// <summary>
 	/// 810 黑板异或游戏
 	/// </summary>
 	/// <param name="nums"></param>
